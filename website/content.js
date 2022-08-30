a53a18bb44897b9d35f5096441ff352a51d70cc1
Deepak Kumar <codeonhunt@gmail.com>
2022-08-31 00:53:11 +0530

    Update content.js

--- website/content.js
+++ website/content.js
@@ -60,57 +60,161 @@ const myFunction = () => {
 
 
 
+////
+
+          getPagination('#myTable');
+					//getPagination('.table-class');
+					//getPagination('table');
+
+		  /*					PAGINATION 
+		  - on change max rows select options fade out all rows gt option value mx = 5
+		  - append pagination list as per numbers of rows / max rows option (20row/5= 4pages )
+		  - each pagination li on click -> fade out all tr gt max rows * li num and (5*pagenum 2 = 10 rows)
+		  - fade out all tr lt max rows * li num - max rows ((5*pagenum 2 = 10) - 5)
+		  - fade in all tr between (maxRows*PageNum) and (maxRows*pageNum)- MaxRows 
+		  */
+		 
+
+function getPagination(table) {
+  var lastPage = 1;
+
+  $('#maxRows')
+    .on('change', function(evt) {
+      //$('.paginationprev').html('');						// reset pagination
+
+     lastPage = 1;
+      $('.pagination')
+        .find('li')
+        .slice(1, -1)
+        .remove();
+      var trnum = 0; // reset tr counter
+      var maxRows = parseInt($(this).val()); // get Max Rows from select option
+
+      if (maxRows == 5000) {
+        $('.pagination').hide();
+      } else {
+        $('.pagination').show();
+      }
+
+      var totalRows = $(table + ' tbody tr').length; // numbers of rows
+      $(table + ' tr:gt(0)').each(function() {
+        // each TR in  table and not the header
+        trnum++; // Start Counter
+        if (trnum > maxRows) {
+          // if tr number gt maxRows
+
+          $(this).hide(); // fade it out
+        }
+        if (trnum <= maxRows) {
+          $(this).show();
+        } // else fade in Important in case if it ..
+      }); //  was fade out to fade it in
+      if (totalRows > maxRows) {
+        // if tr total rows gt max rows option
+        var pagenum = Math.ceil(totalRows / maxRows); // ceil total(rows/maxrows) to get ..
+        //	numbers of pages
+        for (var i = 1; i <= pagenum; ) {
+          // for each page append pagination li
+          $('.pagination #prev')
+            .before(
+              '<li data-page="' +
+                i +
+                '">\
+								  <span>' +
+                i++ +
+                '<span class="sr-only">(current)</span></span>\
+								</li>'
+            )
+            .show();
+        } // end for i
+      } // end if row count > max rows
+      $('.pagination [data-page="1"]').addClass('active'); // add active class to the first li
+      $('.pagination li').on('click', function(evt) {
+        // on click each page
+        evt.stopImmediatePropagation();
+        evt.preventDefault();
+        var pageNum = $(this).attr('data-page'); // get it's number
+
+        var maxRows = parseInt($('#maxRows').val()); // get Max Rows from select option
+
+        if (pageNum == 'prev') {
+          if (lastPage == 1) {
+            return;
+          }
+          pageNum = --lastPage;
+        }
+        if (pageNum == 'next') {
+          if (lastPage == $('.pagination li').length - 2) {
+            return;
+          }
+          pageNum = ++lastPage;
+        }
+
+        lastPage = pageNum;
+        var trIndex = 0; // reset tr counter
+        $('.pagination li').removeClass('active'); // remove active class from all li
+        $('.pagination [data-page="' + lastPage + '"]').addClass('active'); // add active class to the clicked
+        // $(this).addClass('active');					// add active class to the clicked
+	  	limitPagging();
+        $(table + ' tr:gt(0)').each(function() {
+          // each tr in table not the header
+          trIndex++; // tr index counter
+          // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
+          if (
+            trIndex > maxRows * pageNum ||
+            trIndex <= maxRows * pageNum - maxRows
+          ) {
+            $(this).hide();
+          } else {
+            $(this).show();
+          } //else fade in
+        }); // end of for each tr in table
+      }); // end of on click pagination list
+	  limitPagging();
+    })
+    .val(5)
+    .change();
+
+  // end of on select change
+
+  // END OF PAGINATION
+}
+
+function limitPagging(){
+	// alert($('.pagination li').length)
+
+	if($('.pagination li').length > 7 ){
+			if( $('.pagination li.active').attr('data-page') <= 3 ){
+			$('.pagination li:gt(5)').hide();
+			$('.pagination li:lt(5)').show();
+			$('.pagination [data-page="next"]').show();
+		}if ($('.pagination li.active').attr('data-page') > 3){
+			$('.pagination li:gt(0)').hide();
+			$('.pagination [data-page="next"]').show();
+			for( let i = ( parseInt($('.pagination li.active').attr('data-page'))  -2 )  ; i <= ( parseInt($('.pagination li.active').attr('data-page'))  + 2 ) ; i++ ){
+				$('.pagination [data-page="'+i+'"]').show();
+
+			}
+
+		}
+	}
+}
+
+$(function() {
+  // Just to append id number for each row
+  $('table tr:eq(0)').prepend('<th> Sr. No. </th>');
+
+  var id = 0;
+
+  $('table tr:gt(0)').each(function() {
+    id++;
+    $(this).prepend('<td>' + id + '</td>');
+  });
+});
+
+//  Developed By Yasser Mas
+// yasser.mas2@gmail.com
 
 
-<!--<script>-->
-
-<!--frappe.ready(function() {-->
-<!--	var next_start = 20;-->
-<!--	var result_wrapper = $(".website-list .result");-->
-
-<!--	$(".website-list .btn-more").on("click", function() {-->
-<!--		var btn = $(this);-->
-<!--		var data = $.extend(frappe.utils.get_query_params(), {-->
-<!--			doctype: "Mentee",-->
-<!--			txt: "",-->
-<!--			limit_start: next_start,-->
-<!--			pathname: location.pathname,-->
-<!--		});-->
-<!--		data.web_form_name = frappe.web_form_name;-->
-<!--		data.pathname = location.pathname;-->
-<!--		btn.prop("disabled", true);-->
-<!--		return $.ajax({-->
-<!--			url:"/api/method/frappe.www.list.get",-->
-<!--			data: data,-->
-<!--			statusCode: {-->
-<!--				200: function(data) {-->
-<!--					var data = data.message;-->
-<!--					next_start = data.next_start;-->
-<!--					$.each(data.result, function(i, d) {-->
-<!--						$(d).appendTo(result_wrapper);-->
-<!--					});-->
-<!--					toggle_more(data.show_more);-->
-<!--				}-->
-<!--			}-->
-<!--		}).always(function() {-->
-<!--			btn.prop("disabled", false);-->
-<!--		});-->
-<!--	});-->
-<!--	var toggle_more = function(show) {-->
-<!--		if (!show) {-->
-<!--			$(".website-list .more-block").addClass("hide");-->
-<!--		}-->
-<!--	};-->
-
-<!--	if($('.navbar-header .navbar-toggle:visible').length === 1)-->
-<!--	{-->
-<!--		$('.page-head h1').addClass('list-head').click(function(){-->
-<!--			window.history.back();-->
-<!--	 	});-->
-<!--	}-->
-<!--});-->
-
-
-<!--</script>-->
 
 
